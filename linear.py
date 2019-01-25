0338b94ffdd5fe4590aa51b4026d85fded02a43e
semihM <smhmmc98@gmail.com>
2019-01-25 20:46:32 +0300

    Added LU decomposition

--- linear.py
+++ linear.py
@@ -51,12 +51,42 @@ class linearEq:
                 print(" "*(tab_size-len(str(num))),num,end="|")   
                 
             print(" "*(tab_size-len(str(self.consts[rows][0]))),self.consts[rows][0])
-        
-e1=linearEq(Matrix(8,ranged=[-5,5]),Matrix([8,1],ranged=[-25,50]))
-e1.eq
+
 
 def LU(coef_matrix,const_matrix):
-    pass
+    try:
+        assert coef_matrix.dim[0]==coef_matrix.dim[1] and coef_matrix.dim[1]==const_matrix.dim[0] and const_matrix.dim[1]==1
+        L=coef_matrix.lowtri.copy
+        U=coef_matrix.uptri.copy
+        #UX=Y & LY=B
+        X=FMatrix(dim=[L.dim[0],1],randomFill=0,decimal=8)
+        Y=FMatrix(dim=[U.dim[0],1],randomFill=0,decimal=8)
+        B=const_matrix.copy
+    except AttributeError:
+        print("System has infinite solutions")
+        return "Inf"
+    except AssertionError:
+        print("Matrices should be in nxn and nx1 dimensions")
+    else:
+        print("")
+        #Solve LY=B using forward substitution
+        for ys in range(B.dim[0]):
+            Y[ys][0]=B[ys][0]
+            for rest in range(ys):
+                Y[ys][0]-=L[ys][ys-rest-1]*Y[ys-rest-1][0]
+        #Solve UX=Y using backward substitution
+        for xs in range(1,Y.dim[0]+1):
+            X[-xs][0]=Y[-xs][0]/U[-xs][-xs]
+            for rest in range(1,xs):
+                X[-xs][0]-=U[-xs][-rest]*X[-rest][0]/U[-xs][-xs]
+        for i in range(X.dim[0]):
+            print("x{} =".format(i),X[i][0])
+        return X
+
+print("Data table:")
+e1=linearEq(Matrix(5,ranged=[-5,5]),Matrix([5,1],ranged=[-15,10]))
+e1.eq    
+answer=LU(e1.coefs,e1.consts)
 
 def Gauss_Siebel(coef_matrix,const_matrix,init_pred=None,error_upper_bound=1e-2):
     """
