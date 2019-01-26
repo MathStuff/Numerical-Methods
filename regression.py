3a51f9a647867137e676ba57db23d8994f1d3407
semihM <smhmmc98@gmail.com>
2019-01-26 13:14:21 +0300

    Initial commit for regression models

---
+++ regression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 13:09:52 2019
+
+@author: Semih
+
+Models in this module:
+    
+-Linear regression (intercept & no intercept)
+-Power regression
+-Exponential regression
+    
+"""
