edbfd46c66c2f0d2c33c86346c449f3f3b8ff376
semihM <smhmmc98@gmail.com>
2019-02-02 00:47:52 +0300

    Added the base for MatLab

---
+++ non-linear/non_linear.m
@@ -0,0 +1,41 @@
+function root = bisection(fun,low,up,iters)
+%Bisection method to find the root of a function
+%   fun:function
+%   low:lower bound
+%   upper:upper bound
+%   iters:iteration amount  
+%   EXAMPLE:
+%       bisection_answer=bisection(@(a) a*a*a-29,0,4,16)
+    %%%%%CHECK_INTERVAL%%%%%
+    if fun(low)*fun(up)>0
+        fprintf("Not in the interval");
+    end
+
+    while iters>0
+        %%%%%FOUND%%%%%%
+        if fun(low)==0
+            root=low;
+            break
+        end
+
+        if fun(up)==0
+            root=up;
+            break
+        end
+        %%%%%%%%%%%%%%%%
+
+        %%%%ESTIMATE%%%%
+        root=(low+up)/2;
+
+        if fun(low)*fun(root)<0
+            up=root;
+        end
+
+        if fun(root)*fun(up)<0
+            low=root;
+        end
+        iters=iters-1;
+        %%%%%%%%%%%%%%%%
+    end
+end
+
