c6b3a11486e778d7fb88a30923340812e96c0aab
semihM <smhmmc98@gmail.com>
2019-02-02 19:23:00 +0300

    Updated secant method

--- non-linear/MATLAB_R2018b/secant_method.m
+++ non-linear/MATLAB_R2018b/secant_method.m
@@ -1,5 +1,5 @@
 function root = secant_method(fun,g1,g2,iters)
-%Newton-Raphson method to find an estimation of the root of a function
+%Secant method to find an estimation of the root of a function
 %fun:function
 %g1:initial guess 1
 %g2:initial guess 2
